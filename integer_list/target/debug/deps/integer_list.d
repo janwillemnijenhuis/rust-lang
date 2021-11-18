4d3d3af05ab02a97306440108ac6186e6ee8cf5a
Jan Willem Nijenhuis <janwillem.nijenhuis@nedap.com>
2021-11-18 08:58:43 +0100

    integer list crate initialized

---
+++ integer_list/target/debug/deps/integer_list.d
@@ -0,0 +1,5 @@
+C:\Users\janwillem.nijenhuis\Documents\rust-lang\integer_list\target\debug\deps\integer_list.exe: src\main.rs
+
+C:\Users\janwillem.nijenhuis\Documents\rust-lang\integer_list\target\debug\deps\integer_list.d: src\main.rs
+
+src\main.rs:
---
+++ integer_list/target/debug/integer_list.d
@@ -0,0 +1,1 @@
+C:\Users\janwillem.nijenhuis\Documents\rust-lang\integer_list\target\debug\integer_list.exe: C:\Users\janwillem.nijenhuis\Documents\rust-lang\integer_list\src\main.rs
