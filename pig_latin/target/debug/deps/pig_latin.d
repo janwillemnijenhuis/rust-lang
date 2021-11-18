365e1f200abebdb3dc2ab4df968bc91144c4c67f
Jan Willem Nijenhuis <janwillem.nijenhuis@nedap.com>
2021-11-18 17:11:08 +0100

    added pig-latin

---
+++ pig_latin/target/debug/deps/pig_latin.d
@@ -0,0 +1,5 @@
+C:\Users\janwillem.nijenhuis\Documents\rust-lang\pig_latin\target\debug\deps\pig_latin.exe: src\main.rs
+
+C:\Users\janwillem.nijenhuis\Documents\rust-lang\pig_latin\target\debug\deps\pig_latin.d: src\main.rs
+
+src\main.rs:
---
+++ pig_latin/target/debug/pig_latin.d
@@ -0,0 +1,1 @@
+C:\Users\janwillem.nijenhuis\Documents\rust-lang\pig_latin\target\debug\pig_latin.exe: C:\Users\janwillem.nijenhuis\Documents\rust-lang\pig_latin\src\main.rs
