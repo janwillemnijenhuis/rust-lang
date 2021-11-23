ce197b8abb99fd870e2f3611690eda86a59e5774
Jan Willem Nijenhuis <janwillem.nijenhuis@nedap.com>
2021-11-23 16:41:27 +0100

    finished chapter 11, init chapter 12

---
+++ crates/ch11_adder/src/lib.rs
@@ -0,0 +1,131 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_adds_two(){
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 1,
+            height: 5,
+        };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Jan Willem");
+        assert!(result.contains("Jan Willem"), "Greeting did not contain name, value was: `{}`", result);
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value was 200, must be smaller than 100.")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        }
+        else {
+            Err(String::from("two plus two does not equal four."))
+        }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10();
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    #[ignore]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10();
+        assert_eq!(5, value);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value was {}, must be greater than 1.", value);
+        }
+        else if value > 100 {
+            panic!("Guess value was {}, must be smaller than 100.", value);
+        }
+        Guess{value}
+    }
+}
+
+fn prints_and_returns_10() -> i32 {
+    10
+}
