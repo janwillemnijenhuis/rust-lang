fd08a86c27e78d209e7d75f0e5554d5859b54f18
Jan Willem Nijenhuis <janwillemnijenhuis@gmail.com>
2021-11-30 17:37:30 +0100

    added 13.1

--- crates/workout/src/main.rs
+++ crates/workout/src/main.rs
@@ -1,3 +1,79 @@
+use std::thread;
+use std::time::Duration;
+use std::collections::HashMap;
+use std::collections::hash_map::{Entry, OccupiedEntry, VacantEntry};
+use std::hash::Hash;
+
 fn main() {
-    println!("Hello, world!");
+    let simulated_user_specified_value = 6;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_result.value(intensity));
+        }
+    }
+}
+
+struct Cacher<T,U,V>
+where
+    T: Fn(U) -> V,
+{
+    calculation: T,
+    value: HashMap<U,V>,
+}
+
+impl<T,U,V> Cacher<T,U,V>
+where
+    T: Fn(U) -> V,
+    U: Eq + Hash + Clone,
+{
+    fn new(calculation: T) -> Self {
+        Cacher {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: U) -> &V {
+        self.value
+            .entry(arg.clone())
+            .or_insert_with(|| (self.calculation)(arg))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(*v2, 2);
+    }
 }
